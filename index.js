cf6c7dda280abc77308cdc4875a19f021e7320f3
Jeremy <jplatt4427@gmail.com>
2022-04-08 11:54:16 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(dNames, drivers){
+    const results = dNames.filter(nameTest => nameTest == drivers);
+    return results;
+}
+
+function fuzzyMatch(dNames, drivers){
+    let dr = drivers.split();
+        let results = dNames.filter(nameTest => nameTest.slice(0,2) == dr.slice(0,2));
+        return results
+    
+}
+
+function matchName(dObj, drivers){
+    const results = dObj.filter(nameTest => nameTest.name === drivers);
+    return results;
+}
